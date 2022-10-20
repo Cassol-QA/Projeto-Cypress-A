571030e35aeebefff8ca99b8104e9ed0bbddab33
Jovani Miguel Custodio <qacassol@gmail.com>
2022-10-19 23:42:10 -0300

    Subindo a estrutura inicial do projeto

---
+++ cypress/integration/PrimeiroTeste.espec.js
@@ -0,0 +1,15 @@
+///<reference types ="cypress"/>
+
+describe ("Automação Web Cypress ",()=>{
+    
+    // be for it → antes de executar qualquer comando ele é acessado várias vezes 
+    before (()=>{
+        cy.visit ("http://ticket-box.s3.eu-central-1.amazonaws.com/index.html")
+    })
+
+    describe ("",()=>{
+        it ("",()=>{
+
+        })
+    })
+})
